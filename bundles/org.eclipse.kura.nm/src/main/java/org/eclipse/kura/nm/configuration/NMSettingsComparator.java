446bd48f208365ff1d5c7b44031a0410edae0780
Mattia Dal Ben <matthewdibi@gmail.com>
2026-02-03 08:39:40 +0100

    docs: add documentation on comparison method

--- bundles/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/configuration/NMSettingsComparator.java
+++ bundles/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/configuration/NMSettingsComparator.java
@@ -6,6 +6,14 @@ import org.freedesktop.dbus.types.Variant;
 
 public class NMSettingsComparator {
 
+    /* This method compares two NM connection settings maps and determines if they are equal. This comparison
+     * is asymmetric, meaning that if newConnectionSettings contains all the settings in oldConnectionSettings with the same
+     * values, it is considered equal, even if oldConnectionSettings has additional settings.
+     *
+     * @param newConnectionSettings The new connection settings to compare.
+     * @param oldConnectionSettings The old connection settings to compare against. Can be a superset of newConnectionSettings.
+     * @return true if the settings are considered equal, false otherwise.
+     */
     public static boolean areSettingsEqual(Map<String,Map<String,Variant<?>>> newConnectionSettings,
             Map<String,Map<String,Variant<?>>> oldConnectionSettings) {
         // TODO Auto-generated method stub
