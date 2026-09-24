1a9e5343a691e7f3f3715c1b4db025d5646f56ec
Mattia Dal Ben <matthewdibi@gmail.com>
2026-09-24 11:18:15 +0200

    style: fix copyright headers

--- org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/util/DhcpLeaseTool.java
+++ org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/util/DhcpLeaseTool.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022, 2023 Sterwen Technology and others
+ * Copyright (c) 2022, 2026 Sterwen Technology and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
